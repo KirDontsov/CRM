f1a34b106560a680111a9246d3b4f31d9186d48d
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2022-12-25 20:39:01 +0300

    fix: eslint
    STAGE-1

--- server/dist/users/dto/create-user.input.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateUserInput {
-    username: string;
-    password: string;
-}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,16 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       sortSchema: true,
